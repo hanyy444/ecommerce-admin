f290266cf69546f2af6dbcce9d3f5180138236e9
ahmedhaanyy <a.hany@complade.com>
2024-01-21 04:11:45 +0200

    fixed origin policy

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -6,7 +6,8 @@ import prismadb from "@/lib/prismadb";
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400"
 }
 
 export async function OPTIONS(){
